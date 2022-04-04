300100a18809cde48b771f83d01ca528efbc612e
Mohammad Rheza Zamani <56064595+rhezazz@users.noreply.github.com>
2022-04-04 20:42:48 +0700

    Delete fun_obj.m

--- fun_obj.m
+++
@@ -1,8 +0,0 @@
-%Objective Function
-function [misfit] = fun_obj(Us_obs, Us_cal)
-    ls = length(Us_obs);
-    for j = 1 : ls
-        m(j) = ((Us_obs(j) - Us_cal(j)))^2;
-    end
-    misfit = sqrt((1/ls)*sum(m));
-end
