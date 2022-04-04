d8e75a1803d80771e8b84fe683a019aca6eb18b3
Mohammad Rheza Zamani <56064595+rhezazz@users.noreply.github.com>
2022-04-04 20:42:33 +0700

    Delete cal_slip.m

--- cal_slip.m
+++
@@ -1,6 +0,0 @@
-%Calculated afterslip  (exponent + Logarithm)
-function [Us] = cal_slip(A,B,C,D,V0,Ta,Tb,Tc,t)
-    for i = 1 : length(t)
-        Us(i) = A.*log(1 + (t(i)./Ta)) + B - C.*exp(-t(i)./Tb) - D.*exp(-t(i)/Tc) + V0;
-    end
-end
